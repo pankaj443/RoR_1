c42f555ecb7872df3790ed4868d1d7b8565f1cf0
EC2 Default User <ec2-user@ip-172-31-76-78.ec2.internal>
2020-12-26 10:33:09 +0000

    create update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,11 +3,55 @@ class ArticlesController < ApplicationController
         @article = Article.new
     end
     
+    
+    def edit
+        
+        @article = Article.find(params[:id])
+        
+    end
+    
+    
+    
+    def update
+        
+        @article = Article.find(params[:id])
+        
+        if @article.update(article_params)
+            
+            flash[:notice] = "UPDATED"
+            redirect_to article_path(@article)
+            
+        else
+            
+            render 'edit'
+            
+        end
+        
+    end
+    
     def create
         #render plain:params[:article].inspect
         @article = Article.new(article_params)
-        @article.save
-        redirect_to_articles_show(@article)
+        
+        if @article.save
+            
+            flash[:notice] = "SAVED"
+            redirect_to article_path(@article)
+            
+        else
+            
+            render 'new'
+            
+        end
+            
+    
+       
+    end
+    
+    
+    def show
+        
+        @article = Article.find(params[:id])
     end
     
     private

21d2ed3583a5c246d89582263e89417ba8289b59
pankaj443 <pankajofficial443@gmail.com>
2020-12-27 11:40:01 +0000

    pass auth

--- Gemfile
+++ Gemfile
@@ -10,7 +10,7 @@ end
 gem 'rails', '~> 5.0.7', '>= 5.0.7.2'
 # Use sqlite3 as the database for Active Record
 
-
+gem 'bcrypt', '~> 3.1.7'
 gem 'bootstrap-sass', '~> 3.4.1'
 # Use Puma as the app server
 gem 'puma', '~> 3.0'
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201225180025) do
+ActiveRecord::Schema.define(version: 20201227113635) do
 
   create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text   "description"
+    t.string  "title"
+    t.text    "description"
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end

fda5dad4f4066dc34de61e046755edf2a64f6aa6
pankaj443 <pankajofficial443@gmail.com>
2020-12-27 16:58:51 +0000

    completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -94,7 +94,7 @@ class ArticlesController < ApplicationController
         
         def require_same_user
             
-            if current_user != @article.user
+            if current_user != @article.user && !current_user.admin?
                 flash[:danger] = "Can only edit your own article"
                 redirect_to root_path
             end

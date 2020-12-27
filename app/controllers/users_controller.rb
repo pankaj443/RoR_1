a8b2e26d6735c62438db561d85fe75a1431651cb
pankaj443 <pankajofficial443@gmail.com>
2020-12-27 14:27:03 +0000

    login logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,12 @@
 class UsersController < ApplicationController
     
+    before_action :set_user, only: [:edit, :update, :show]
+    def index
+        @users = User.paginate(page: params[:page],per_page: 5)
+        
+    end
     
-    
-    def new
+    def news
         
         @user = User.new
         
@@ -24,12 +28,13 @@ class UsersController < ApplicationController
     
     
     def edit 
-        @user = User.find(params[:id])
+       
+        
     end
     
     def update
         
-        @user = User.find(params[:id])
+      
         
         if @user.update(user_params)
             flash[:success]="Updated #{@user.username}"
@@ -46,7 +51,8 @@ class UsersController < ApplicationController
     
     
     def show
-        @user = User.find(params[:id])
+       
+        @user_articles = @user.articles.paginate(page: params[:page],per_page: 5)
     end
     
         
@@ -56,6 +62,11 @@ class UsersController < ApplicationController
     
     def user_params
             params.require(:user).permit(:username,:email,:password)
+    end
+    
+    def set_user
+        
+        @user  =User.find(params[:id])
     end
 
 end
